a48cab22251c8decb2af64587e220c67f8fd98d9
Connor Couetil <couetilc@gmail.com>
2025-04-11 14:30:24 -0400

    hello world server

--- wwwgo/www.go
+++ wwwgo/www.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"html/template"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	http.HandleFunc("/", home)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func home(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	fmt.Fprintln(res, "Hello, world!")
 }
