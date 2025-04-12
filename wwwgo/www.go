fd24eb56f800dcf73f95d32a284ea19592e91d6d
Connor Couetil <couetilc@gmail.com>
2025-04-12 08:35:40 -0400

    simple template for home page

--- wwwgo/www.go
+++ wwwgo/www.go
@@ -5,9 +5,19 @@ import (
 	"os"
 	"net/http"
 	"html/template"
+	"embed"
 )
 
+//go:embed templates
+var templatesFS embed.FS
+var templates *template.Template
+
+func init() {
+	templates = template.Must(template.ParseFS(templatesFS, "templates/*.tmpl"))
+}
+
 func main() {
+	fmt.Println(templates.DefinedTemplates())
 	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
@@ -15,6 +25,8 @@ func main() {
 }
 
 func run() error {
+	// TODO: 404 page.
+	// TODO: proper home page, transfer styles and assets
 	http.HandleFunc("/", home)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -27,5 +39,5 @@ func run() error {
 func home(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(http.StatusOK)
-	fmt.Fprintln(res, "Hello, world!")
+	templates.ExecuteTemplate(res, "home.tmpl", nil)
 }
