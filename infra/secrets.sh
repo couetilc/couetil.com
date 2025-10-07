f6c9e19de571e3c23a1f010b7d739dd19304c0b9
Connor Couetil <couetilc@gmail.com>
2025-10-07 15:33:21 -0400

    delete old secrets file

--- infra/secrets.sh
+++
@@ -1,6 +0,0 @@
-echo '{
-  "server_public_key": "{{ op://couetil.com/wireguard/server/public_key }}",
-  "server_private_key": "{{ op://couetil.com/wireguard/server/private_key }}",
-  "client_public_key": "{{ op://couetil.com/wireguard/client/public_key }}",
-  "client_private_key": "{{ op://couetil.com/wireguard/client/private_key }}"
-}' | op inject
